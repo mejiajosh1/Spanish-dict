34315baad598efedf04276822d6ca5aba9a243c8
mejiajosh1 <mejiajoshua1@gmail.com>
2015-05-03 16:34:00 -0700

    Flashcards can be created, edited, and deleted.

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -7,25 +7,46 @@ function Todos($element) {
   var collection = [];
   var $el = $element;
 
-  // Handle marking todo as "done"
-  $el.on('click', 'input[type=checkbox]', function() {
+  // Remove current flashcard from database.
+  $el.on('click', 'button.removecard', function() {
     hoodie.store.remove('todo', $(this).parent().data('id'));
     return false;
   });
 
-  // Handle "inline editing" of a todo.
-  $el.on('click', 'label', function() {
+  // Displays field for editing of selected card.
+  $el.on('click', 'button.editcard', function() {
     $(this).parent().parent().find('.editing').removeClass('editing');
     $(this).parent().addClass('editing');
+    var englishword = $(this).parent().find(".englishword").text();
+    var spanishword = $(this).parent().find(".spanishword").text();
+    $(".new-englishword").val(englishword);
+    $(".new-spanishword").val(spanishword);
+
     return false;
   });
 
-  // Handle updating of an "inline edited" todo.
-  $el.on('keypress', 'input[type=text]', function(event) {
-    if (event.keyCode === 13) {
-      hoodie.store.update('todo', $(this).parent().data('id'), {title: event.target.value});
-    }
+  // Creates flashcard.
+  $("#addcard").on('click', function(event) {
+    var english_word = $('#englishword').val();
+    var spanish_word = $('#spanishword').val();
+    hoodie.store.add('todo', {
+      english: english_word,
+      spanish: spanish_word,
+    });
+  })
+
+  // Saves changes.
+  $el.on('click', 'button.savecard', function() {
+    var newenglish_word = $(this).parent().find('.new-englishword').val();
+    var newspanish_word = $(this).parent().find('.new-spanishword').val();
+    var id = $(this).parent().data('id');
+    hoodie.store.update('todo', id,  {
+      english: newenglish_word,
+      spanish: newspanish_word,
+    });
+    $(this).parent().removeClass('editing');
   });
+
 
   // Find index/position of a todo in collection.
   function getTodoItemIndexById(id) {
@@ -37,6 +58,7 @@ function Todos($element) {
     return null;
   }
 
+  //Displays markup of flashcards.
   function paint() {
     $el.html('');
     collection.sort(function(a, b) {
@@ -44,10 +66,15 @@ function Todos($element) {
     });
     for (var i = 0, len = collection.length; i<len; i++) {
       $el.append(
-        '<li data-id="' + collection[i].id + '">' +
-          '<input type="checkbox"> <label>' + collection[i].title + '</label>' +
-          '<input type="text" value="' + collection[i].title + '"/>' +
-        '</li>'
+        '<div class="panel panel-default lead text-center" data-id="' + collection[i].id + '">' +
+          '<hr><div class = "englishword hide-edit">' + collection[i].english + '</div>' +
+          '<input type="text" class="form-control show-edit new-englishword" /><hr>' +
+          '<div class = "spanishword hide-edit">' + collection[i].spanish + '</div>' +
+          '<input type ="text" class="form-control show-edit new-spanishword" /><hr>' +
+          '<button class="removecard hide-edit" class="btn btn-primary">Remove Card</button>' +
+          '<button class="editcard hide-edit" class="btn btn-primary">Edit Card</button>' +
+          '<button class="savecard show-edit" class="btn btn-primary">Save Card</button>' +
+        '</div>'
       );
     }
   }
@@ -87,13 +114,3 @@ hoodie.store.on('todo:update', todos.update);
 hoodie.store.on('todo:remove', todos.remove);
 // clear todos when user logs out,
 hoodie.account.on('signout', todos.clear);
-
-
-// handle creating a new task
-$('#todoinput').on('keypress', function(event) {
-  // ENTER & non-empty.
-  if (event.keyCode === 13 && event.target.value.length) {
-    hoodie.store.add('todo', {title: event.target.value});
-    event.target.value = '';
-  }
-});
